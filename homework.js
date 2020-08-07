cce083879c9bab24f104e0269eedea046f1e5efb
Mairuwka <Maiorov23@yandex.ru>
2020-08-07 13:59:39 +0300

    feat: Solved the first task

--- homework.js
+++ homework.js
@@ -1,3 +1,9 @@
-for (var i = 0; i < 5; i++) {
-		 console.log(i);
+// Напишите функцию sumTo(n), которая для данного n вычисляет сумму чисел от 1 до n
+function sumTo(n) {
+	let sum = 0;
+	for (let i = 1; i <= n; i++) {
+		sum += i;
+	}
+	return sum;
 }
+console.log(sumTo(5));
