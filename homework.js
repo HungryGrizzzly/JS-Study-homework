13e98d4bc84bb0e5ddb871508aa8872c79a5aa9d
Mairuwka <Maiorov23@yandex.ru>
2020-08-07 14:06:04 +0300

    feat: Solved the second task

--- homework.js
+++ homework.js
@@ -7,3 +7,16 @@ function sumTo(n) {
 	return sum;
 }
 console.log(sumTo(5));
+
+// Как исправить код ниже, чтобы он вывел в консоль 5 разных значений (от 0 до 4):
+/*for (var i = 0; i < 5; i++) {
+	setTimeout(function() {
+		 console.log(i);
+	}, 1000);
+} */
+
+for (let i = 0; i < 5; i++) {
+	setTimeout(function () {
+		console.log(i);
+	}, 1000);
+}
